c65b36d3d96649d9e5255449f7c84aee3fd1f787
Rohit Sharma <rohit.sharma.working@gmail.com>
2026-06-11 23:48:47 +0530

    Update TextEditor.js

--- frontend/src/TextEditor.js
+++ frontend/src/TextEditor.js
@@ -1,11 +1,1 @@
-import React from 'react';
-
-function TextEditor() {
-  return (
-    <div>
-      <h2>Text Editor Component</h2>
-    </div>
-  );
-}
-
-export default TextEditor;
+console.log("Text Editor")
